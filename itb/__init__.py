7ae33f9ac1018913ae41997ec01e3ea4dc492f3c
Andrey Kuvshinov <andrew@encloud.blue>
2026-09-03 09:05:52 +0700

    sync from monorepo 1e18b2e
    Source: github.com/everanium/itb @ 1e18b2e75412ebfdc8a35dc8f8b9b59bec048789

--- itb/__init__.py
+++ itb/__init__.py
@@ -27,7 +27,7 @@ from .runtime import set_gc_percent, set_memory_limit, version
 from .status import Status
 from .stream import DecryptStream, EncryptStream
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 __all__ = [
     "DecryptStream",
